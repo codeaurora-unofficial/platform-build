3bd01de365322bad4ba1f70598fb6b56e8f7ce51
Qualcomm Innovation Center <initial-contribution@quicinc.com>
2009-05-02 16:11:00 -0700

    Initial Contribution

--- core/main.mk
+++ core/main.mk
@@ -360,6 +360,7 @@ INTERNAL_DEFAULT_DOCS_TARGETS :=
 subdirs := \
 	bionic \
 	system/core \
+	bootable/bootloader/legacy \
 	build/libs \
 	build/target \
 	build/tools/acp \
@@ -369,7 +370,8 @@ subdirs := \
 	external/elfcopy \
 	external/elfutils \
 	external/yaffs2 \
-	external/zlib
+	external/zlib \
+	frameworks/base/libs/utils
 else	# !BUILD_TINY_ANDROID
 
 #
