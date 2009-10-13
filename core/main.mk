959e7440bc740e950646d668e32bf6dc540564d2
David Ng <dng@quicinc.com>
2009-10-13 14:31:52 -0700

    build: Define TARGET_BOOTLOADER_BOARD_NAME
    Define TARGET_BOOTLOADER_BOARD_NAME if not already defined
    to ensure ro.product.board property is properly defined.

--- core/main.mk
+++ core/main.mk
@@ -450,6 +450,9 @@ TARGET_DEVICE_DIR := $(patsubst %/,%,$(dir $(board_config_mk)))
 board_config_mk :=
 
 # Clean up/verify variables defined by the board config file.
+ifeq ($(TARGET_BOOTLOADER_BOARD_NAME),)
+  TARGET_BOOTLOADER_BOARD_NAME := $(TARGET_DEVICE)
+endif
 TARGET_BOOTLOADER_BOARD_NAME := $(strip $(TARGET_BOOTLOADER_BOARD_NAME))
 #TARGET_CPU_ABI := $(strip $(TARGET_CPU_ABI))
 #ifeq ($(TARGET_CPU_ABI),)
