477497ee30c8cbbb5f5c936cbbafaa45133e2f42
Naseer Ahmed <naseer@qualcomm.com>
2010-04-23 20:30:30 +0530

    build: Turn off checkjni by default
    Turn off checkjni property so that the hard limit of
    2000 jni references does not interfere with monkey runs.
    To enable at runtime "setprop dalvik.vm.checkjni false"
    can be used later on.
    
    Change-Id: Ic3bde8fcdd3cca40c1cf5d7c77154faa9d9a25c2

--- core/main.mk
+++ core/main.mk
@@ -206,8 +206,9 @@ ifneq (,$(user_variant))
   ADDITIONAL_DEFAULT_PROPERTIES += ro.allow.mock.location=0
   
 else # !user_variant
-  # Turn on checkjni for non-user builds.
-  ADDITIONAL_BUILD_PROPERTIES += ro.kernel.android.checkjni=1
+  # Turn off checkjni for non-user builds.
+  # Use "setprop dalvik.vm.checkjni" false to enable it at runtime
+  ADDITIONAL_BUILD_PROPERTIES += ro.kernel.android.checkjni=0
   # Set device insecure for non-user builds.
   ADDITIONAL_DEFAULT_PROPERTIES += ro.secure=0
   # Allow mock locations by default for non user builds
