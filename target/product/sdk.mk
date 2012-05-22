ce6989a5f0bbc854834d3c0e63a571b8e4464fb9
Michael Wu <mwu@mozilla.com>
2012-05-21 23:26:06 -0400

    Make sdk and development repos optional

--- target/product/sdk.mk
+++ target/product/sdk.mk
@@ -62,8 +62,8 @@ PRODUCT_PACKAGES := \
 	Calendar
 
 # Define the host tools and libs that are parts of the SDK.
-include sdk/build/product_sdk.mk
-include development/build/product_sdk.mk
+-include sdk/build/product_sdk.mk
+-include development/build/product_sdk.mk
 
 # audio libraries.
 PRODUCT_PACKAGES += \
