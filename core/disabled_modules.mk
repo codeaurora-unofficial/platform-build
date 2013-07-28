1157d9efbe45bc2abb7e1e4ab7bf3653714c5051
Michael Wu <mwu@mozilla.com>
2013-07-28 17:00:19 -0400

    Support building B2G on JB MR2

---
+++ core/disabled_modules.mk
@@ -0,0 +1,15 @@
+DISABLED_USER_MODULES := \
+	libtestplugin \
+	temp_layoutlib \
+	libmock_ril \
+	libI420colorconvert \
+	apkcheck \
+	elftree \
+	jdwpspy \
+	a3dconvert \
+	libsampleplugin \
+	libstagefright_chromium_http \
+	libjavacore \
+	app_process__asan \
+	xaplay \
+
