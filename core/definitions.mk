35a5c992b62468b927a388b9112452a71c939e85
Diego Wilson <dwilson@codeaurora.org>
2013-12-19 14:13:33 -0800

    Disable building libdrmframework_jni.
    This is Java native interface libraries which is not required in B2G.
    
    Change-Id: I6ed30e995c2541852cb1440e973ce420c47f1e18

--- core/definitions.mk
+++ core/definitions.mk
@@ -2238,6 +2238,7 @@ DISABLED_USER_MODULES := \
 	app_process__asan \
 	libnative-media-jni \
 	xaplay \
+	libdrmframework_jni \
 
 # broken:
 #	$(foreach file,$^,$(if $(findstring,.a,$(suffix $file)),-l$(file),$(file)))
