0d6696703b4af818df80d7de2b51c668a1b6948e
Chuck Lee <chulee@mozilla.com>
2014-11-07 14:41:59 +0800

    Bug 1084232 - Enable internal EAP server config of hostapd.

--- target/board/generic/BoardConfig.mk
+++ target/board/generic/BoardConfig.mk
@@ -92,3 +92,17 @@ ifeq ($(TARGET_PRODUCT),sdk)
   # include an expanded selection of fonts for the SDK.
   EXTENDED_FONT_FOOTPRINT := true
 endif
+
+# Build flags for Wifi Marionette Test
+BOARD_WPA_SUPPLICANT_DRIVER := TEST
+BOARD_HOSTAPD_DRIVER := TEST
+WPA_SUPPLICANT_VERSION := VER_0_8_X
+
+CONFIG_EAP := true
+CONFIG_EAP_PEAP := true
+CONFIG_EAP_TTLS := true
+CONFIG_EAP_TLS := true
+
+WIFI_DRIVER_FW_PATH_AP := "ap"
+WIFI_DRIVER_FW_PATH_STA := "sta"
+WIFI_DRIVER_FW_PATH_PARAM := "/data/misc/wifi/fake_fwpath"
