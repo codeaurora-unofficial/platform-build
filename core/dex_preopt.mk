617416c605adf11170ceb8855541e6436803bf83
Nagender Telkar <ntelkar@codeaurora.org>
2014-12-02 19:19:06 -0700

    Update DEXPREOPT_BOOT_JARS to replace space with :s
    This will make it easier to extend PRODUCT_BOOT_JARS in the
    product configuration files.
    
    Change-Id: I261f735344828d227a1764a90c2fc37f47e361ce

--- core/dex_preopt.mk
+++ core/dex_preopt.mk
@@ -3,7 +3,7 @@
 #
 ####################################
 
-DEXPREOPT_BOOT_JARS := $(PRODUCT_BOOT_JARS)
+DEXPREOPT_BOOT_JARS :=  $(subst $(space),:,$(PRODUCT_BOOT_JARS))
 DEXPREOPT_BOOT_JARS_MODULES := $(subst :, ,$(DEXPREOPT_BOOT_JARS))
 PRODUCT_BOOTCLASSPATH := $(subst $(space),:,$(foreach m,$(DEXPREOPT_BOOT_JARS_MODULES),/system/framework/$(m).jar))
 
