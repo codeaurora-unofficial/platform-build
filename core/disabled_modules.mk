dd56575fd5e9a5d288184a35c6ad51ccf082bbb7
Michael Wu <mwu@mozilla.com>
2014-12-16 15:34:28 -0500

    Update package lists for l

--- core/disabled_modules.mk
+++ core/disabled_modules.mk
@@ -8,9 +8,10 @@ DISABLED_USER_MODULES := \
 	jdwpspy \
 	a3dconvert \
 	libsampleplugin \
-	libstagefright_chromium_http \
+	libstagefright_http_support \
 	libjavacore \
 	app_process__asan \
 	xaplay \
 	librs_jni \
+	libandroid_runtime \
 
