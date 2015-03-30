885fb762e57ebd482ae63d8e5dfcb44434e7c79f
Sergi Sevil <sergi.sevil@gi-de.com>
2015-03-30 09:17:25 +0200

    Add MscTemrinal to core.mk

--- target/product/core.mk
+++ target/product/core.mk
@@ -24,6 +24,7 @@ PRODUCT_PACKAGES += \
     AssdTerminal \
     eSETerminal \
     UiccTerminal \
+    MscTerminal \
     org.simalliance.openmobileapi \
     org.simalliance.openmobileapi.xml \
     BasicDreams \
