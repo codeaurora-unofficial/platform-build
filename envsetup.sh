5be2c5e2bcb16f862db4733c7d459e9f9d576f7f
Sourabh Banerjee <sbanerje@codeaurora.org>
2016-05-15 23:00:41 -0700

    Revert "add hardware directory as a searchpath for vendorsetup.sh"
    This reverts commit 0ad26ee767647e9dcb6748357188432ab2a6f7c6.
    
    Change-Id: I7e61c60f6320f4998f0f4181012882d951b20634

--- envsetup.sh
+++ envsetup.sh
@@ -1639,8 +1639,7 @@ fi
 # Execute the contents of any vendorsetup.sh files we can find.
 for f in `test -d device && find -L device -maxdepth 4 -name 'vendorsetup.sh' 2> /dev/null | sort` \
          `test -d vendor && find -L vendor -maxdepth 4 -name 'vendorsetup.sh' 2> /dev/null | sort` \
-         `test -d product && find -L product -maxdepth 4 -name 'vendorsetup.sh' 2> /dev/null | sort` \
-         `test -d hardware && find -L hardware -maxdepth 6 -name 'vendorsetup.sh' 2> /dev/null | sort`
+         `test -d product && find -L product -maxdepth 4 -name 'vendorsetup.sh' 2> /dev/null | sort`
 do
     echo "including $f"
     . $f
