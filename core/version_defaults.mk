91ac1ce053557ba2b286f0b79fd6cd317423dd94
c_sbiman <sbiman@codeaurora.org>
2016-12-20 17:12:53 +0530

    Update security patch string to 2016-11-06
    Change-Id: I7824375db0a692e6369ec97a54a825804f203254

--- core/version_defaults.mk
+++ core/version_defaults.mk
@@ -104,7 +104,7 @@ ifeq "" "$(PLATFORM_SECURITY_PATCH)"
   #
   # If there is no $PLATFORM_SECURITY_PATCH set, keep it empty.
 
-  PLATFORM_SECURITY_PATCH := 2016-06-01
+  PLATFORM_SECURITY_PATCH := 2016-11-06
 endif
 
 ifeq "" "$(PLATFORM_BASE_OS)"
