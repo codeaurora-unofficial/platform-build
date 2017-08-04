f062ebde05a8684cd455b76d67bea8fb47d11008
Shibin George <shibing@codeaurora.org>
2017-08-04 20:56:19 +0530

    Upgrade to python3 from python2 for all OTA scripts
    Prerequisite: python3 should be present on the system
    where update.zip generation is taking place.
    
    Summary of the changes:
    -> print() in python is not a statement but a function.
    -> dict.iteritems() is replaced by dict.items().
    -> "None" in python2 was comparable to int, but not anymore.
       sort() needed to be rewritten to accomodate this.
    -> split() works on bytes and not str object in python3.
    
    Change-Id: If80928c7e6f9b0121be5334f69d347c60f102d09

--- tools/releasetools/common.py
+++ tools/releasetools/common.py
@@ -79,7 +79,7 @@ def Run(args, **kwargs):
   """Create and return a subprocess.Popen object, printing the command
   line on the terminal if -v was specified."""
   if OPTIONS.verbose:
-    print "  running: ", " ".join(args)
+    print ("  running: ", " ".join(args))
   return subprocess.Popen(args, **kwargs)
 
 
@@ -116,7 +116,7 @@ def LoadInfoDict(input_file):
           raise KeyError(fn)
   d = {}
   try:
-    d = LoadDictionaryFromLines(read_helper("META/misc_info.txt").split("\n"))
+    d = LoadDictionaryFromLines(read_helper("META/misc_info.txt").decode().split("\n"))
   except KeyError:
     # ok if misc_info.txt doesn't exist
     pass
@@ -187,9 +187,9 @@ def LoadBuildProp(read_helper):
   try:
     data = read_helper("SYSTEM/build.prop")
   except KeyError:
-    print "Warning: could not find SYSTEM/build.prop in %s" % zip
+    print ("Warning: could not find SYSTEM/build.prop in %s" % zip)
     data = ""
-  return LoadDictionaryFromLines(data.split("\n"))
+  return LoadDictionaryFromLines(data.decode().split("\n"))
 
 def LoadDictionaryFromLines(lines):
   d = {}
@@ -215,12 +215,12 @@ def LoadRecoveryFSTab(read_helper, fstab_version):
   try:
     data = read_helper("RECOVERY/recovery.fstab")
   except KeyError:
-    print "Warning: could not find RECOVERY/recovery.fstab"
+    print ("Warning: could not find RECOVERY/recovery.fstab")
     data = ""
 
   if fstab_version == 1:
     d = {}
-    for line in data.split("\n"):
+    for line in data.decode().split("\n"):
       line = line.strip()
       if not line or line.startswith("#"):
         continue
@@ -247,7 +247,7 @@ def LoadRecoveryFSTab(read_helper, fstab_version):
           if i.startswith("length="):
             length = int(i[7:])
           else:
-            print "%s: unknown option \"%s\"" % (mount_point, i)
+            print ("%s: unknown option \"%s\"" % (mount_point, i))
 
       d[mount_point] = Partition(mount_point=mount_point, fs_type=pieces[1],
                                  device=pieces[2], length=length,
@@ -299,7 +299,7 @@ def LoadRecoveryFSTab(read_helper, fstab_version):
 
 def DumpInfoDict(d):
   for k, v in sorted(d.items()):
-    print "%-25s = (%s) %s" % (k, type(v).__name__, v)
+    print ("%-25s = (%s) %s" % (k, type(v).__name__, v))
 
 
 def BuildBootableImage(sourcedir, fs_config_file, info_dict=None):
@@ -423,15 +423,15 @@ def GetBootableImage(name, prebuilt_name, unpack_dir, tree_subdir,
 
   prebuilt_path = os.path.join(unpack_dir, "BOOTABLE_IMAGES", prebuilt_name)
   if os.path.exists(prebuilt_path):
-    print "using prebuilt %s from BOOTABLE_IMAGES..." % (prebuilt_name,)
+    print ("using prebuilt %s from BOOTABLE_IMAGES..." % (prebuilt_name,))
     return File.FromLocalFile(name, prebuilt_path)
 
   prebuilt_path = os.path.join(unpack_dir, "IMAGES", prebuilt_name)
   if os.path.exists(prebuilt_path):
-    print "using prebuilt %s from IMAGES..." % (prebuilt_name,)
+    print ("using prebuilt %s from IMAGES..." % (prebuilt_name,))
     return File.FromLocalFile(name, prebuilt_path)
 
-  print "building image from target_files %s..." % (tree_subdir,)
+  print ("building image from target_files %s..." % (tree_subdir,))
   fs_config = "META/" + tree_subdir.lower() + "_filesystem_config.txt"
   data = BuildBootableImage(os.path.join(unpack_dir, tree_subdir),
                             os.path.join(unpack_dir, fs_config),
@@ -605,10 +605,10 @@ def CheckSize(data, target, info_dict):
     raise ExternalError(msg)
   elif pct >= 95.0:
     print
-    print "  WARNING: ", msg
+    print ("  WARNING: ", msg)
     print
   elif OPTIONS.verbose:
-    print "  ", msg
+    print ("  ", msg)
 
 
 def ReadApkCerts(tf_zip):
@@ -657,8 +657,8 @@ COMMON_DOCSTRING = """
 """
 
 def Usage(docstring):
-  print docstring.rstrip("\n")
-  print COMMON_DOCSTRING
+  print (docstring.rstrip("\n"))
+  print (COMMON_DOCSTRING)
 
 
 def ParseOptions(argv,
@@ -682,7 +682,7 @@ def ParseOptions(argv,
         list(extra_long_opts))
   except getopt.GetoptError as err:
     Usage(docstring)
-    print "**", str(err), "**"
+    print ("**", str(err), "**")
     sys.exit(2)
 
   for o, a in opts:
@@ -778,7 +778,7 @@ class PasswordManager(object):
         current[i] = ""
 
       if not first:
-        print "key file %s still missing some passwords." % (self.pwfile,)
+        print ("key file %s still missing some passwords." % (self.pwfile,))
         answer = raw_input("try to edit again? [y]> ").strip()
         if answer and answer[0] not in 'yY':
           raise RuntimeError("key passwords unavailable")
@@ -838,13 +838,13 @@ class PasswordManager(object):
           continue
         m = re.match(r"^\[\[\[\s*(.*?)\s*\]\]\]\s*(\S+)$", line)
         if not m:
-          print "failed to parse password file: ", line
+          print ("failed to parse password file: ", line)
         else:
           result[m.group(2)] = m.group(1)
       f.close()
     except IOError as e:
       if e.errno != errno.ENOENT:
-        print "error reading password file: ", str(e)
+        print ("error reading password file: ", str(e))
     return result
 
 
@@ -945,7 +945,7 @@ class DeviceSpecificParams(object):
     """Keyword arguments to the constructor become attributes of this
     object, which is passed to all functions in the device-specific
     module."""
-    for k, v in kwargs.iteritems():
+    for k, v in kwargs.items():
       setattr(self, k, v)
     self.extras = OPTIONS.extras
 
@@ -962,10 +962,10 @@ class DeviceSpecificParams(object):
           if x == ".py":
             f = b
           info = imp.find_module(f, [d])
-        print "loaded device-specific extensions from", path
+        print ("loaded device-specific extensions from", path)
         self.module = imp.load_module("device_specific", *info)
       except ImportError:
-        print "unable to load device-specific module; assuming none"
+        print ("unable to load device-specific module; assuming none")
 
   def _DoCall(self, function_name, *args, **kwargs):
     """Call the named function in the device-specific module, passing
@@ -1096,7 +1096,7 @@ class Difference(object):
       th.start()
       th.join(timeout=300)   # 5 mins
       if th.is_alive():
-        print "WARNING: diff command timed out"
+        print ("WARNING: diff command timed out")
         p.terminate()
         th.join(5)
         if th.is_alive():
@@ -1104,8 +1104,8 @@ class Difference(object):
           th.join()
 
       if err or p.returncode != 0:
-        print "WARNING: failure running %s:\n%s\n" % (
-            diff_program, "".join(err))
+        print ("WARNING: failure running %s:\n%s\n" % (
+            diff_program, "".join(err)))
         self.patch = None
         return None, None, None
       diff = ptemp.read()
@@ -1127,11 +1127,15 @@ class Difference(object):
 
 def ComputeDifferences(diffs):
   """Call ComputePatch on all the Difference objects in 'diffs'."""
-  print len(diffs), "diffs to compute"
+  print (len(diffs), "diffs to compute")
 
   # Do the largest files first, to try and reduce the long-pole effect.
   by_size = [(i.tf.size, i) for i in diffs]
-  by_size.sort(reverse=True)
+
+  # sort only by the first element of the pair i.e. the tf size
+  # to avoid python3's "unorderable type" errors
+  by_size.sort(reverse=True, key=lambda x:x[0])
+
   by_size = [i[1] for i in by_size]
 
   lock = threading.Lock()
@@ -1153,13 +1157,13 @@ def ComputeDifferences(diffs):
         else:
           name = "%s (%s)" % (tf.name, sf.name)
         if patch is None:
-          print "patching failed!                                  %s" % (name,)
+          print ("patching failed!                                  %s" % (name,))
         else:
-          print "%8.2f sec %8d / %8d bytes (%6.2f%%) %s" % (
-              dur, len(patch), tf.size, 100.0 * len(patch) / tf.size, name)
+          print ("%8.2f sec %8d / %8d bytes (%6.2f%%) %s" % (
+              dur, len(patch), tf.size, 100.0 * len(patch) / tf.size, name))
       lock.release()
     except Exception as e:
-      print e
+      print (e)
       raise
 
   # start worker threads; wait for them all to finish.
@@ -1434,9 +1438,9 @@ fi
         m = re.match(r"^service flash_recovery /system/(\S+)\s*$", line)
         if m:
           sh_location = m.group(1)
-          print "putting script in", sh_location
+          print ("putting script in", sh_location)
           break
   except (OSError, IOError) as e:
-    print "failed to read init.rc: %s" % (e,)
+    print ("failed to read init.rc: %s" % (e,))
 
   output_sink(sh_location, sh)
