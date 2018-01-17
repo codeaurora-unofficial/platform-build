489d2639a564548bbeefcccaf0a416d41d544713
Chennuru Rajeshwar Reddy <rchenn@codeaurora.org>
2018-01-17 11:17:44 +0530

    Update Security patch string date to 2017-12-05
    Change-Id: Iabce6700fc2732dff28efdc91f5fec55484a748c

--- core/version_defaults.mk
+++ core/version_defaults.mk
@@ -104,7 +104,7 @@ ifeq "" "$(PLATFORM_SECURITY_PATCH)"
   #
   # If there is no $PLATFORM_SECURITY_PATCH set, keep it empty.
 
-  PLATFORM_SECURITY_PATCH := 2017-11-06
+  PLATFORM_SECURITY_PATCH := 2017-12-05
 endif
 
 ifeq "" "$(PLATFORM_BASE_OS)"
