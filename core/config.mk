d35cd605a54731ec6e5225a157af7993dbdef5d6
Dan Willemsen <dwillemsen@google.com>
2018-02-26 18:26:44 -0800

    Use xmllint built from source
    Instead of relying on whatever version of xmllint is on the host system,
    build and use the version in external/libxml2.
    
    Test: diff build_aosp-marlin.ninja, expected changes.
    Test: m $(xmllint targets in build_aosp-marlin.ninja)
    Test: introduce xml error, build fails
    Change-Id: I39579f06db3777e3b5c8dda7c7541c25a35887b2
    (cherry picked from commit 4c36d45a2cc03ce76307c1183831e622e7325e26)

--- core/config.mk
+++ core/config.mk
@@ -558,6 +558,7 @@ IJAR := $(prebuilt_build_tools_bin)/ijar
 MAKEPARALLEL := $(prebuilt_build_tools_bin)/makeparallel
 SOONG_JAVAC_WRAPPER := $(SOONG_HOST_OUT_EXECUTABLES)/soong_javac_wrapper
 SOONG_ZIP := $(SOONG_HOST_OUT_EXECUTABLES)/soong_zip
+XMLLINT := $(SOONG_HOST_OUT_EXECUTABLES)/xmllint
 ZIP2ZIP := $(SOONG_HOST_OUT_EXECUTABLES)/zip2zip
 ZIPTIME := $(prebuilt_build_tools_bin)/ziptime
 
