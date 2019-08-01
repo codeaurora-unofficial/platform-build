d4b2dc4f9d56c62884c9228d2a2b4420ddf5aee4
Xiao Li <lixiao@codeaurora.org>
2019-08-01 15:34:30 +0800

    core_base.mk: modify default notification_sound/alarm_alert
    Change notification_sound and alarm_alert since the original one
    does not exist in system.
    
    Change-Id: I2e13a0a0e3492e6bab695441118607b078fb7a3b

--- target/product/core_base.mk
+++ target/product/core_base.mk
@@ -17,8 +17,8 @@
 # that should not be in PDK should be added in lower level like core.mk.
 
 PRODUCT_PROPERTY_OVERRIDES := \
-    ro.config.notification_sound=OnTheHunt.ogg \
-    ro.config.alarm_alert=Alarm_Classic.ogg
+    ro.config.notification_sound=Argon.ogg \
+    ro.config.alarm_alert=Alarm_Beep_03.ogg
 
 PRODUCT_PACKAGES += \
     ContactsProvider \
