a7c51b83f0df8892327939a109823dbde27f7008
Marco Zhang <zhangx@codeaurora.org>
2020-11-13 18:54:48 +0800

    Revert "Bug 105275 - add fota build part: generate oem_fota_meta.zip."
    This reverts commit c599c1d0d25aded563c5162dc2abede37c41cf2e.
    
    Change-Id: Iee9d59924414cfbceac15543dcc9f56934620d58

--- tools/releasetools/common.py
+++ tools/releasetools/common.py
@@ -1520,8 +1520,6 @@ def ZipWrite(zip_file, filename, arcname=None, perms=0o644,
   if arcname is None:
     arcname = filename
 
-  if os.path.exists(filename) is False:
-    return
   saved_stat = os.stat(filename)
 
   try:
